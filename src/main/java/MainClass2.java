8e8b5071e5940ea6100e5288409af77e9096bff4
mis94 <mis1494@gmail.com>
2018-02-27 14:39:00 +0200

    trying zookeeper

--- src/main/java/MainClass2.java
+++ src/main/java/MainClass2.java
@@ -2,22 +2,14 @@ import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
 import io.vertx.core.eventbus.EventBusOptions;
 import io.vertx.core.spi.cluster.ClusterManager;
-import io.vertx.spi.cluster.ignite.IgniteClusterManager;
-import org.apache.ignite.configuration.IgniteConfiguration;
-import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi;
-import org.apache.ignite.spi.discovery.tcp.ipfinder.multicast.TcpDiscoveryMulticastIpFinder;
-
-import java.util.Arrays;
+import io.vertx.spi.cluster.zookeeper.ZookeeperClusterManager;
 
 public class MainClass2 {
     public static void main(String[] args) {
-        IgniteConfiguration cfg = configureIgnite();
-        ClusterManager igniteClusterManager = new IgniteClusterManager(cfg);
+        ClusterManager zookeeperClusterManager = new ZookeeperClusterManager();
 
         // EventBusOptions eventBusOptions = configureEventBus();
-        VertxOptions options = new VertxOptions().setClusterHost("172.19.1.56").setClusterPort(8081)
-                .setClusterPublicHost("172.19.1.56")
-                .setClusterManager(igniteClusterManager);
+        VertxOptions options = new VertxOptions().setClusterManager(zookeeperClusterManager).setClustered(true);
 
         Vertx.clusteredVertx(options, res -> {
             if (res.succeeded()) {
@@ -43,20 +35,5 @@ public class MainClass2 {
         EventBusOptions eventBusOptions = new EventBusOptions()
                 .setClusterPublicHost("172.19.1.56").setClusterPublicPort(8082);
         return eventBusOptions;
-    }
-
-    private static IgniteConfiguration configureIgnite() {
-        TcpDiscoverySpi spi = new TcpDiscoverySpi();
-        TcpDiscoveryMulticastIpFinder ipFinder = new TcpDiscoveryMulticastIpFinder();
-        // Set Multicast group.
-//        ipFinder.setMulticastGroup("228.10.10.157");
-        // Set initial IP addresses.
-        // Note that you can optionally specify a port or a port range.
-        // here add the address of the other machine
-        ipFinder.setAddresses(Arrays.asList("172.19.1.56", "172.19.1.56:47500..47509", "172.19.1.13", "172.19.1.13:47500..47509"));
-        spi.setIpFinder(ipFinder);
-        IgniteConfiguration cfg = new IgniteConfiguration();
-        cfg.setDiscoverySpi(spi);
-        return cfg;
     }
 }
